e252ba727cb4cb31f9fc6fadaead1f1f7938e366
yauvink <yau.vink@gmail.com>
2018-06-24 17:34:00 +0300

    add mobile menu

--- js/my-js2.js
+++ js/my-js2.js
@@ -1,1 +1,7 @@
-$('.menu-btn').on('click', function(e) {e.preventDefault;$(this).toggleClass('menu-btn_active');});
+$('.menu-btn').on('click', function(e) {
+	e.preventDefault();
+	$(this).toggleClass('menu-btn_active');});
+$('.menu-btn').on('click', function(e) {
+	e.preventDefault();
+	$('.menu-mobile').toggleClass('menu-mobile_active');
+})
