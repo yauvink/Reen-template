218095a8473c3fc1e59dcd882be08e9bf58cb71a
yauvink <yau.vink@gmail.com>
2018-06-25 23:41:08 +0300

    add slider

--- js/my-js2.js
+++ js/my-js2.js
@@ -1,7 +1,11 @@
 $('.menu-btn').on('click', function(e) {
 	e.preventDefault();
-	$(this).toggleClass('menu-btn_active');});
-$('.menu-btn').on('click', function(e) {
-	e.preventDefault();
+	$(this).toggleClass('menu-btn_active');
 	$('.menu-mobile').toggleClass('menu-mobile_active');
-})
+});
+$('.my-slider').slick({
+			arrows: false,
+			dots: true,
+			slidesToShow: 4,
+			slidesToScroll: 1
+});
